25139ef64ea458ac0835feeb77590868815b79f2
Máté Sárdi <mate.sardi@mozaweb.com>
2021-08-22 16:59:42 +0200

    feat: put custom header into notion cover

--- wrangler/notion/src/customScript.ts
+++ wrangler/notion/src/customScript.ts
@@ -1,11 +1,36 @@
 export default () => {
     const moveHeader = () => {
         const notionScroller = document.getElementsByClassName("notion-scroller")[0] as HTMLDivElement;
-        if (!(notionScroller?.children[0]?.classList.contains("mattsml-header") ?? true)) {
-            const header = document.getElementsByClassName("mattsml-header")[0].cloneNode(true) as HTMLDivElement;
-            header.classList.remove("mattsml-header--hidden");
-            notionScroller.prepend(header as any);
+
+        if (notionScroller == null) {
+            return;
         }
+
+        const coverImage = notionScroller.querySelector(":scope > div:first-child img") as HTMLImageElement;
+
+        if (coverImage == null) {
+            return;
+        }
+
+        if (!coverImage.src.includes("solid_blue.png")) {
+            return;
+        }
+
+        const headersPlace = notionScroller.querySelector(
+            ":scope > div:first-child > div:first-child > div:first-child",
+        );
+
+        if (headersPlace == null) {
+            return;
+        }
+
+        if (headersPlace.classList.contains("mattsml-header")) {
+            return;
+        }
+
+        const header = document.getElementsByClassName("mattsml-header")[0].cloneNode(true) as HTMLDivElement;
+        header.classList.remove("mattsml-header--hidden");
+        headersPlace.replaceWith(header);
     };
 
     (window as any).customMutationScript = () => {
