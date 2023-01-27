95fdfba0e571fe57b1b1e1be7ec7b2f133d23ea7
Matt Sárdi <sardi.matt@gmail.com>
2023-01-27 15:20:44 +0100

    chore: enable dotenv-webpack

--- wrangler/notion/webpack.config.js
+++ wrangler/notion/webpack.config.js
@@ -1,4 +1,5 @@
 const path = require('path')
+const Dotenv = require('dotenv-webpack');
 
 module.exports = {
   entry: './src/index.ts',
@@ -31,4 +32,7 @@ module.exports = {
       },
     ],
   },
+  plugins: [
+    new Dotenv(),
+  ],
 }
