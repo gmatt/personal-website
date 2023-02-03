06b8f9193dc47f1be9ab0c7bf88fe93b86e89650
Matt Sárdi <sardi.matt@gmail.com>
2023-02-03 10:55:01 +0100

    fix: bug with google analytics

--- wrangler/notion/src/index.ts
+++ wrangler/notion/src/index.ts
@@ -227,7 +227,7 @@ class BodyRewriter {
             window.dataLayer = window.dataLayer || [];
             function gtag(){dataLayer.push(arguments);}
             gtag('js', new Date());
-            gtag('config', ${GA_TAG});
+            gtag('config', '${GA_TAG}');
             </script>${
                 ENVIRONMENT === "dev"
                     ? `
